b96e5724958097c3a5372bf35568e1dabf6876f8
unknown <demeshkotetjana@gmail.com>
2021-05-26 12:17:38 +0300

    JavaScriptTest

---
+++ index.js
@@ -0,0 +1,28 @@
+// task 4 function
+
+// let myArray = new Array('Wind', 'Rain', 'Fire')
+// function sort() {
+//   myArray.sort()
+//    console.log(myArray)
+// }
+// sort();
+
+
+// let a1 = ['a', 10, 'b', 20, 'c', 30]
+
+//   let a2 = a1.filter(function(item) {return typeof item ==='string';})
+//   console.log(a2);
+
+//   let a3 = a1.filter(function(item) {return typeof item ==='number';})
+//   console.log(a3)
+
+// / task 5 function
+
+let myArray = ["1", "2", "3" ]
+function concat () {
+   myArray = myArray.concat('4', '5', '6')
+ console.log(myArray)
+
+}
+
+concat () 
